af7195f8301d2edca44dcaad4048cf4abad45670
Ayub Bhatta <118649309+ayubhatta@users.noreply.github.com>
2025-02-02 11:35:00 +0545

    added other apis

--- src/api/api.js
+++ src/api/api.js
@@ -93,3 +93,93 @@ export const paginationApi = (page, limit) =>
 export const bikeCount = () => {
   return Api.get(`/api/bike/bike_count`, config);
 };
+// =============================== Bike Parts API ===========================================
+export const createBikePartsApi = (data) =>
+  FileApi.post('/api/bikeParts/create', data);
+
+export const getAllBikePartsApi = () => Api.get('/api/bikeParts', config);
+
+export const updateBikePartsApi = (id, data) =>
+  FileApi.put(`/api/bikeParts/${id}`, data, config);
+
+// ================================ Cart Api =============
+export const addToCartApi = (data) => Api.post('/api/cart/add', data, config);
+
+export const getCartApi = () => Api.get('/api/cart/user', config);
+
+export const deleteCartApi = (userId = userID) =>
+  Api.delete(`/api/cart/delete/${userId}`, config);
+
+export const deleteCartItemApi = (cartid) => {
+  return Api.delete(`/api/cart/${cartid}`, config);
+};
+
+export const updateCartApi = (cartid, data) =>
+  Api.put(`/api/cart/${cartid}`, data, config);
+
+// =============================== Bookings API ===========================================
+
+// Add to Boking API
+export const addToBookingApi = (data) =>
+  Api.post('/api/booking/add', data, config);
+
+// Display Booking
+export const getAllBookingApi = () => Api.get('/api/booking/getall', config);
+
+// Delete Booking
+export const deleteBookingApi = (id) => Api.delete(`/api/booking/delete/${id}`);
+
+// User Booking
+export const userBookingApi = (userID) =>
+  Api.get(`/api/booking/getall/${userID}`, config);
+
+export const cancelBookingApi = (id) => {
+  return Api.post(`/api/booking/cancel/${id}`, config);
+};
+
+// ============================= Admin Panel ===========================================
+
+export const getDashboardStats = () =>
+  Api.get('/api/admin/dashboard_stats', config);
+
+// ================================= Message API ===========================================
+export const sendMessageApi = (message) =>
+  Api.post('/api/messages/send', message, config);
+export const getMessagesApi = (id, page) =>
+  Api.get(`/api/messages/get/${id}?page=${page}`, config);
+
+export const getByIdApi = (id) =>
+  Api.get(`/api/messages/get_by_id/${id}`, config);
+
+export const updateMessageApi = (id, message) =>
+  Api.put(`/api/messages/update/${id}`, message, config);
+
+export const deleteMessageApi = (id) =>
+  Api.delete(`/api/messages/delete/${id}`, config);
+
+export const getChatListApi = () => Api.get(`/api/messages/get`, config);
+
+export const sendFileApi = (data, config) =>
+  Api.post(`/api/messages/send/file`, data, config);
+
+// ================================== Message File ==========================
+export const messageFileUrl = 'http://localhost:5000/messages/files';
+export const messageImageUrl = 'http://localhost:5000/messages/images';
+
+// =============================== Notification =========================
+export const getNotificationApi = () =>
+  Api.get(`/api/notifications/get`, config);
+
+export const readNotificationApi = (id) =>
+  Api.put(`/api/notifications/read/${id}`, config);
+
+// ================================ send notification =======================
+export const sendNotificationApi = (data) =>
+  Api.post(`/api/notifications/send`, data, jsonConfig);
+
+// ================================ Feedback API ==============================
+
+export const sendFeedbackApi = (data) =>
+  Api.post('/api/feedback/postFeedback', data, config);
+
+export const getFeedbackApi = () => Api.get('/api/feedback/all', config);
