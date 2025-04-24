1ae0ffc9b2a69092a005efd07215907a362e757c
Ayub Bhatta <118649309+ayubhatta@users.noreply.github.com>
2025-04-24 22:26:45 +0545

    made changes in the routes

--- src/protected/User/UserRoutes.jsx
+++ src/protected/User/UserRoutes.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const UserRoutes = () => {
   const user = JSON.parse(localStorage.getItem('user'));
 
-  return user != null && user != user.isAdmin ? (
+  return user != null && user.role === 'User' ? (
     <Outlet />
   ) : (
     <Navigate to={'/login'} />
